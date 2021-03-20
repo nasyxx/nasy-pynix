2db067b90fb617dd06901d9f4e009a349b08fa6d
Nasy <nasyxx+git@gmail.com>
2021-03-20 18:16:24 -0400

    New args.
    * app.nix
    * default.nix
    
    Now it accepts a latest_stdenv, a packageOverlay an ignore check packages list.

--- app.nix
+++ app.nix
@@ -1,10 +1,6 @@
 { python, pkgs }:
 
 let
-  py = import (./. + "/python-modules") {
-    inherit pkgs;
-    python = python;
-  };
   pypkgs = python.pkgs;
   toApp = n: pypkgs.toPythonApplication pypkgs.${n};
   toApps = apps: builtins.listToAttrs (map (app: { name = app; value = toApp app; }) apps);
--- default.nix
+++ default.nix
@@ -1,19 +1,67 @@
 { pkgs ? import <nixpkgs> { }
 , python ? "python3"
+, latest_stdenv ? true
+, packageOverrides ? (n: o: { })
+, ignoreChecks ? [ ]
 }:
 
 let
-  _python = pkgs."${python}";
-  _pypkgs = import (./. + "/python-modules") {
-    inherit pkgs;
-    python = _python;
+  _stdenv = if latest_stdenv then pkgs.llvmPackages_latest.stdenv else pkgs.stdenv;
+  _python = pkgs."${python}".override {
+    self = _python;
+    stdenv = _stdenv;
   };
+  _pypkgs = py: import (./. + "/python-modules") {
+    inherit pkgs;
+    python = py;
+  };
+
+  ignoreCheck = p: p.overridePythonAttrs (_: {
+    doInstallCheck = false;
+    doCheck = false;
+    checkInputs = [ ];
+    installCheckPhase = "";
+  });
+
+  ignoreCheckSet = pyp: ps:
+    builtins.mapAttrs
+      (p: override:
+        pyp.${p}.overridePythonAttrs (_:
+          override // {
+            doInstallCheck = false;
+            doCheck = false;
+            checkInputs = [ ];
+            installCheckPhase = "";
+          }))
+      ps;
+
+  ignoreCheckList = pyp: ps:
+    ignoreCheckSet pyp (builtins.listToAttrs (builtins.map
+      (p: {
+        name = p;
+        value = {
+          doInstallCheck = false;
+          doCheck = false;
+          checkInputs = [ ];
+          installCheckPhase = "";
+        };
+      })
+      ps));
+
+
 in
 rec {
+  inherit ignoreCheck ignoreCheckSet ignoreCheckList;
   inherit pkgs;
-  python = _python;
-  pypkgs = python.pkgs // _pypkgs;
-  apps = {py ? _python}:
-    import ./app.nix { inherit pkgs; python = py;};
-  withPackages = python.withPackages;
+  inherit _python;
+
+
+  python = _python.override {
+    self = python;
+    packageOverrides = (n: o: _pypkgs python // ignoreCheckList o ignoreChecks // packageOverrides n python);
+    stdenv = _stdenv;
+  };
+
+  apps = { py ? python }:
+    import ./app.nix { inherit pkgs; python = py; };
 }
